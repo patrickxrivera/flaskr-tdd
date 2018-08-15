886dd9f5714d8d01f88875136a544605f67eeeda
Patrick Rivera <pxr13@my.fsu.edu>
2018-08-15 08:32:40 -0700

    init repo

--- app_test.py
+++ app_test.py
@@ -1,15 +1,45 @@
-from app import app
-
 import unittest
+import os
+import tempfile
+
+import app
 
 
 class BasicTestCase(unittest.TestCase):
 
     def test_index(self):
-        tester = app.test_client(self)
+        tester = app.app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b'Hello World!')
+
+    def test_db(self):
+        tester = os.path.exists('flaskr.db')
+        self.assertEqual(tester, True)
+
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
+        app.app.config['TESTING'] = True
+        self.app = app.app.test_client()
+        app.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(app.app.config['DATABASE'])
+
+    def login(self, username, password):
+        credentials = dict(username=username, password=password)
+        return self.app.post('/login', data=credentials, follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
+    # assert functions
+
+    def test_empty_db(self):
+        rv = self.app.get('/')
 
 
 if __name__ == '__main__':
